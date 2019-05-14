020427d221fce578f8ad42b7d480f8d03a8d2085
Robin Kurosawa <robin@Robins-MacBook-Pro.local>
2019-05-14 09:17:23 -0400

    added amplify and xcode files, first big commit

---
+++ Open Credit X/Open Credit X/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Open Credit X
+//
+//  Created by Robin Kurosawa on 5/13/19.
+//  Copyright © 2019 Robin Kurosawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Open Credit X/Open Credit XTests/Open_Credit_XTests.swift
@@ -0,0 +1,34 @@
+//
+//  Open_Credit_XTests.swift
+//  Open Credit XTests
+//
+//  Created by Robin Kurosawa on 5/13/19.
+//  Copyright © 2019 Robin Kurosawa. All rights reserved.
+//
+
+import XCTest
+@testable import Open_Credit_X
+
+class Open_Credit_XTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
